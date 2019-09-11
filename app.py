1d4936d6740bff5c72be971862bcbd1f02cff891
riotalienware <riotalienware@gmail.com>
2019-09-11 13:53:24 -0400

    Initial tobii integration attempt

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import time
 import wx
 
+import tobii_research as tr
 from tobii_research import find_all_eyetrackers
 
 class MyEyeTracker:
@@ -11,9 +13,59 @@ class MyEyeTracker:
 
         self.eyetracker = trackers[0]
 
+    def calibrate(self):
+        eyetracker = self.eyetracker
+        calibration = tr.ScreenBasedCalibration(eyetracker)
+
+        calibration.enter_calibration_mode()
+        print("Entered calibration mode for eye tracker with serial number {0}.".format(eyetracker.serial_number))
+
+        # Define the points on screen we should calibrate at.
+        # The coordinates are normalized, i.e. (0.0, 0.0) is the upper left corner and (1.0, 1.0) is the lower right corner.
+        points_to_calibrate = [(0.5, 0.5), (0.1, 0.1), (0.1, 0.9), (0.9, 0.1), (0.9, 0.9)]
+
+        for point in points_to_calibrate:
+            print("Show a point on screen at {0}.".format(point))
+
+            # Wait a little for user to focus.
+            time.sleep(0.7)
+
+            print("Collecting data at {0}.".format(point))
+            if calibration.collect_data(point[0], point[1]) != tr.CALIBRATION_STATUS_SUCCESS:
+                # Try again if it didn't go well the first time.
+                # Not all eye tracker models will fail at this point, but instead fail on ComputeAndApply.
+                calibration.collect_data(point[0], point[1])
+
+        print("Computing and applying calibration.")
+        calibration_result = calibration.compute_and_apply()
+        print("Compute and apply returned {0} and collected at {1} points.".
+              format(calibration_result.status, len(calibration_result.calibration_points)))
+
+        # Analyze the data and maybe remove points that weren't good.
+        recalibrate_point = (0.1, 0.1)
+        print("Removing calibration point at {0}.".format(recalibrate_point))
+        calibration.discard_data(recalibrate_point[0], recalibrate_point[1])
+
+        # Redo collection at the discarded point
+        print("Show a point on screen at {0}.".format(recalibrate_point))
+        calibration.collect_data(recalibrate_point[0], recalibrate_point[1])
+
+        # Compute and apply again.
+        print("Computing and applying calibration.")
+        calibration_result = calibration.compute_and_apply()
+        print("Compute and apply returned {0} and collected at {1} points.".
+              format(calibration_result.status, len(calibration_result.calibration_points)))
+
+        # See that you're happy with the result.
+
+        # The calibration is done. Leave calibration mode.
+        calibration.leave_calibration_mode()
+
+        print("Left calibration mode.")
+
 class MyFrame(wx.Frame):
-    def __init__(self, parent, title, eye_tracker):
-        self.eye_tracker = eye_tracker
+    def __init__(self, parent, title, eyetracker):
+        self.eyetracker = eyetracker
 
         wx.Frame.__init__(self, parent, title=title, size=(200, 100))
 
@@ -22,6 +74,14 @@ class MyFrame(wx.Frame):
 
         self.ShowFullScreen(True)
         self.Show(True)
+
+        print("Initiating calibration")
+        try:
+            self.eyetracker.calibrate()
+        except Exception as e:
+            print("Unable to initiate calibration:")
+            print(e)
+            self.Close()
 
     def CloseFrame(self, event):
         self.Close()
@@ -70,12 +130,12 @@ class MyFrame(wx.Frame):
 
 eyetracker = MyEyeTracker()
 
-app = wx.App(False)
+app = wx.App(redirect=False)
 
 frame = MyFrame(
     parent=None,
     title='Eye-Tracking Calibration',
-    eye_tracker=eyetracker,
+    eyetracker=eyetracker,
 )
 
 app.MainLoop()
