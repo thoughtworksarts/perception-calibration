7837138fefb011c694896efc0625af5b9a388693
JC Holder <jc.holder@thoughtworks.com>
2019-09-17 12:33:20 -0400

    Set constants dynamically

--- constants.py
+++ constants.py
@@ -1,19 +1,26 @@
-import wx
+constants_and_defaults = {
+    # 1."0": perfect aligment,
+    "USER_POSITION_SCORE_REQUIREMENT": 0.85,
 
-USER_POSITION_SCORE_REQUIREMENT = 0.85  # 1.0 = perfect aligment
+    # How many previous scores to sum up when calculating total score
+    # Arbitrary threshold. TODO: Test
+    "USER_POSITION_SCORE_BACK_LOOK": 100,
 
-# How many previous scores to sum up when calculating total score
-USER_POSITION_SCORE_BACK_LOOK = 100  # Arbitrary threshold. TODO: Test
+    # Weights should sum up to 1.0
+    "X_SCORE_WEIGHT": 0.4,
+    "Y_SCORE_WEIGHT": 0.4,
+    "Z_SCORE_WEIGHT": 0.2,
 
-# Weights should sum up to 1.0
-X_SCORE_WEIGHT = 0.4
-Y_SCORE_WEIGHT = 0.4
-Z_SCORE_WEIGHT = 0.2
+    # Scores shrink expontentially worse the farther the head is off target
+    "X_SCORE_EXPONENT": 10,
+    "Y_SCORE_EXPONENT": 10,
+    "Z_SCORE_EXPONENT": 10,
 
-# Scores shrink expontentially worse the farther the head is off target
-X_SCORE_EXPONENT = 10
-Y_SCORE_EXPONENT = 10
-Z_SCORE_EXPONENT = 10
+    "CIRCLE_MARGIN": 20,
+    "CIRCLE_RADIUS": 20,
+}
 
-CIRCLE_MARGIN = 20
-CIRCLE_RADIUS = 20
+for constant_name in constants_and_defaults:
+    default = constants_and_defaults[constant_name]
+
+    globals()[constant_name] = default
